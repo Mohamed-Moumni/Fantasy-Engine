3f4569b2b4602a613530d89ca3e8260cb5548efb
Mohamed-Moumni <mohamedmoumni593@gmail.com>
2026-01-09 15:04:21 +0100

    feat: implemented the score calcualtor for Position: F,D with tests

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,6 @@ import (
 
 	"fantasy-engine/internal/config"
 	"fantasy-engine/internal/database"
-	"fantasy-engine/internal/services"
 )
 
 func main() {
--- internal/services/scoreCalculator.go
+++ internal/services/scoreCalculator.go
@@ -17,7 +17,10 @@ func (sc *ScoreCalculator) CalculatePlayerScore(stats models.MatchStats) int32 {
 		return sc.calculateMidfielderScore(stats)
 	case "G":
 		return sc.calcualtorGoalKeeperScore(stats)
-	// Add other positions like "F", "D", "G" here
+	case "F":
+		return sc.calculateForwardScore(stats)
+	case "D":
+		return sc.calculateDefenderScore(stats)
 	default:
 		return 0
 	}
@@ -36,8 +39,9 @@ func (sc *ScoreCalculator) calculateMidfielderScore(stats models.MatchStats) int
 	score += int32(stats.Goals * 6)
 	score += int32(stats.GoalAssist * 3)
 
-	// clearness + Ball recoveries + interceptions + tackles
-	accumulationBonus := stats.TotalClearance + stats.BallRecovery + stats.InterceptionWon + stats.TotalTackle
+
+	// clearness + OutfielderBlcok +  Ball recoveries + interceptions + tackles
+	accumulationBonus := stats.OutfielderBlock + stats.TotalClearance + stats.BallRecovery + stats.InterceptionWon + stats.TotalTackle
 	if accumulationBonus >= 12 {
 		score += 2
 	}
@@ -82,11 +86,69 @@ func (sc *ScoreCalculator) calcualtorGoalKeeperScore(stats models.MatchStats) in
 	return score
 }
 
+func (sc *ScoreCalculator) calculateForwardScore(stats models.MatchStats) int32 {
+	var score int32 = 0
 
-// func (sc *ScoreCalculator) calculateDefenderScore(stats models.MatchStats) int32 {
+	if stats.MinutesPlayed <= 60 {
+		score += 1
+	} else {
+		score += 2
+	}
 
-// }
+	score += int32(stats.Goals * 4)
+	score += int32(stats.GoalAssist * 3)
 
-// func (sc *ScoreCalculator) calculateForwardScore(stats models.MatchStats) int32 {
+	// clearness + OutfielderBlcok +  Ball recoveries + interceptions + tackles
+	accumulationBonus := stats.OutfielderBlock + stats.TotalClearance + stats.BallRecovery + stats.InterceptionWon + stats.TotalTackle
+	if accumulationBonus >= 12 {
+		score += 2
+	}
 
-// }
+	if stats.YellowCard {
+		score -= 1
+	}
+
+	if stats.RedCard {
+		score -= 3
+	}
+
+	score -= int32(stats.OwnGoals * 2)
+	return score
+}
+
+func (sc *ScoreCalculator) calculateDefenderScore(stats models.MatchStats) int32 {
+	var score int32 = 0
+	
+	if stats.MinutesPlayed <= 60 {
+		score += 1
+	} else {
+		score += 2
+	}
+	score += int32(stats.Goals * 7)
+	score += int32(stats.GoalAssist * 3)
+
+	// clearness + OutfielderBlock +  Ball recoveries + interceptions + tackles
+	accumulationBonus := stats.OutfielderBlock + stats.TotalClearance + stats.BallRecovery + stats.InterceptionWon + stats.TotalTackle
+	if accumulationBonus >= 10 {
+		score += 2
+	}
+
+	if stats.Rating >= 8.0 {
+		score += 2
+	}
+
+	if stats.WonTackle > 5 {
+		score += 2
+	}
+
+	if stats.YellowCard {
+		score -= 1
+	}
+
+	if stats.RedCard {
+		score -= 3
+	}
+
+	score -= int32(stats.OwnGoals * 2)
+	return score
+}
