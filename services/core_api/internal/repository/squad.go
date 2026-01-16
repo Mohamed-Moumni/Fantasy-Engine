fa06345aef3410388dc2313d364db6d0455fb66b
Mohamed-Moumni <mmoumni@student.1337.ma>
2026-01-16 11:40:11 +0100

    refactor(core_api): remove outdated comments from CreateSquad method

--- services/core_api/internal/repository/squad.go
+++ services/core_api/internal/repository/squad.go
@@ -14,9 +14,6 @@ func NewSquadRepository(db *gorm.DB) *SquadRepository {
 	return &SquadRepository{db: db}
 }
 
-// create squad:
-// first create the squad
-// then associate the squad to specific user by updating the user's squad ID
 func (sr *SquadRepository) CreateSquad(squad *models.Squad, userId string) error {
 	result := sr.db.Create(squad)
 	if result.Error != nil {
